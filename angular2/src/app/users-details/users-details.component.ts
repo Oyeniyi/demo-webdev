de240715195ea0b21bc4897be1358c868936b419
oyeniyi3i <sunday.oyeniyi6@gmail.com>
2017-10-29 00:22:26 +0000

    Demonstration d'AngularJS 2

---
+++ angular2/src/app/users-details/users-details.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NgProgress } from 'ngx-progressbar';
+import { ActivatedRoute } from '@angular/router';
+import { AppRequestService } from '../app.service';
+
+declare var google;
+
+@Component({
+  selector: 'app-users-details',
+  templateUrl: './users-details.component.html',
+  styleUrls: ['./users-details.component.css']
+})
+export class UsersDetailsComponent implements OnInit {
+    @ViewChild('map') mapElement: ElementRef;
+
+    idUser: any;
+    user: any;
+    showUser: any = false;
+
+  constructor(private appRequestService: AppRequestService, private activateRoute: ActivatedRoute, public ngProgress: NgProgress) {
+    this.activateRoute.params.subscribe(params => {
+        this.idUser = params['id'];
+    });
+  }
+
+  ngOnInit() {
+      this.ngProgress.start();
+      this.appRequestService.getUserById(this.idUser).subscribe(response => {
+          this.showUser = true;
+          this.user = response;
+
+          // Pour la map
+          console.log(this.user.address.geo.lat);
+          console.log(this.user.address.geo.lng);
+          const myLatlng = new google.maps.LatLng(this.user.address.geo.lng, this.user.address.geo.lat);
+          const mapOptions = {
+              zoom: 13,
+              center: myLatlng,
+              scrollwheel: false
+          };
+          const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+          const Marker = new google.maps.Marker({
+              position: myLatlng,
+              title: 'Hello World!'
+          });
+          // To add the marker to the map, call setMap();
+          Marker.setMap(map);
+
+          this.ngProgress.done();
+      });
+  }
+}
